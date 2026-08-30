5aeb8cc7c7834abf7a064b2d4d67141bb398ec08
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-08-30 16:38:47 +0600

    Metadata layout modified and opengraph image added

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,10 +3,12 @@ import Header from "@/components/layouts/header";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "HomeBuild Construction | Residential & Commercial Construction Services",
+  title:{
+    default:"BuildWell Construction | Residential & Commercial Construction Services",
+    template:"%s | BuildWell Construction"
+  },
   description:
-    "HomeBuild Construction delivers high-quality residential and commercial construction, remodeling, renovations, and custom building solutions. We combine expert craftsmanship, transparent communication, and reliable project management to bring your vision to life.",
+    "BuildWell Construction delivers high-quality residential and commercial construction, remodeling, renovations, and custom building solutions. We combine expert craftsmanship, transparent communication, and reliable project management to bring your vision to life.",
 };
 
 export default function SiteLayout({
