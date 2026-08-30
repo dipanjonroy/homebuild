76811e5d6f02fe467881339011ddb6ea17b52f67
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-08-30 18:04:30 +0600

    fix: make error page static

--- src/app/(public)/[...slug]/page.tsx
+++ src/app/(public)/[...slug]/page.tsx
@@ -1,13 +1,13 @@
-
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.BASE_URL!),
   title: "404-Page not found",
 };
 
+export const dynamic = "force-static";
+
 export default function CatchError() {
-  
   return notFound();
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,12 +3,17 @@ import Header from "@/components/layouts/header";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:{
-    default:"BuildWell Construction | Residential & Commercial Construction Services",
-    template:"%s | BuildWell Construction"
+  metadataBase: new URL(process.env.BASE_URL!),
+  title: {
+    default:
+      "BuildWell Construction | Residential & Commercial Construction Services",
+    template: "%s | BuildWell Construction",
   },
   description:
     "BuildWell Construction delivers high-quality residential and commercial construction, remodeling, renovations, and custom building solutions. We combine expert craftsmanship, transparent communication, and reliable project management to bring your vision to life.",
+  openGraph: {
+    images: "/homepage/homepage-hero-image.jpg",
+  },
 };
 
 export default function SiteLayout({
@@ -20,7 +25,7 @@ export default function SiteLayout({
     <>
       <Header />
       <main>{children}</main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
